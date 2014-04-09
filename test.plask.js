4c458687c3e4766e6d521231c6445b5bcc154e95
Marcin Ignac <vorg@Makowka.local>
2014-04-09 11:36:17 +0100

    Open a window in Plask

---
+++ test.plask.js
@@ -0,0 +1,15 @@
+var sys = require('pex-sys');
+
+sys.Window.create({
+  settings: {
+    width: 1280,
+    height: 720,
+    type: '3d'
+  },
+  init: function() {
+
+  },
+  draw: function() {
+    
+  }
+});
