114f7bc04f483a017a983d448245a2b94fc4d778
Marcin Ignac <vorg@Makowka.local>
2014-04-14 21:36:53 +0100

    Switched from Edges and Faces to arrays of indices

--- build.js
+++ build.js
@@ -3,8 +3,7 @@ var fs = require('fs');
 
 var b = browserify();
 
-//b.add('./test.plask.js');
-b.add('./node_modules/pex-materials/lib/SolidColor.js');
-b.transform('brfs');
+b.add('./test.plask.js');
+b.transform({global:true}, 'brfs');
 b.ignore('plask');
 b.bundle().pipe(fs.createWriteStream('./test.browser.js'));
